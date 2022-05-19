b095bf15efd090b8239f12d80c43989007465544
natkleve <nate.kleven@gmail.com>
2022-05-19 15:23:22 -0700

    Updated meetings table formatting

--- src/controllers/meetingsController.js
+++ src/controllers/meetingsController.js
@@ -1,12 +1,14 @@
 const functions = require('../utils/functions');
 const params = require('../utils/params');
 const webexService = require('../services/webexService');
+const { append } = require('express/lib/response');
 const logger = require('../utils/logger')('meetingsController');
 
 function meetingsController() {
 
     async function getIndex(req, res){
         const me = await webexService.getMe(req.session.access_token);
+        req.session.me = me;
         if (!req.session.meetings){
             res.render('meetings',{
                 title: 'Meeting Manager',
@@ -22,6 +24,11 @@ function meetingsController() {
         if(req.body.meetingHost){
             const meetings = await webexService.listMeetings(req.body.meetingHost, req.session.access_token);
             logger.debug('fetched meetings');
+            res.render('meetings',{
+                title: 'Meeting Manager',
+                me: req.session.me,
+                meetings: meetings
+            })
         }
     }
 
